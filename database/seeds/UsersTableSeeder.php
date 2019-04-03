b4b789f62d0ba3c6310c340e1edd32c07555bef7
Gustavo Sobrinho <gustavo.sobrinho01@gmail.com>
2019-04-03 16:09:58 -0300

    Change email default to candidato@eskive.tech

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,7 +14,7 @@ class UsersTableSeeder extends Seeder
         factory(App\User::class, 1000)->create();
 
         $firstUser = App\User::first();
-        $firstUser->email = 'gsobrinho@f-scp.com';
+        $firstUser->email = 'candidato@eskive.tech';
         $firstUser->save();
 
         dump("FIRST_USER_EMAIL=$firstUser->email");
